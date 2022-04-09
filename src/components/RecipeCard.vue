b6d234982e430ef7f8f29e05f4b759930da37fda
ceciliehrr <ceciliehrr@gmail.com>
2022-04-09 21:49:56 +0200

    add color theme

--- src/components/RecipeCard.vue
+++ src/components/RecipeCard.vue
@@ -1,26 +1,32 @@
 <template>
-  <Card>
-    <template #header>
-      <div class="emoji-container">
-        <img
-          alt="user header"
-          src="https://foodstalker.b-cdn.net/2022/chili-skrei/DSC02431-2.jpg"
-        />
-        <div class="emoji">🌽</div>
-      </div>
-    </template>
+  <a href="" class="no-underline">
+    <Card>
+      <template #header>
+        <div class="emoji-container">
+          <img
+            alt="user header"
+            src="https://foodstalker.b-cdn.net/2022/chili-skrei/DSC02431-2.jpg"
+          />
+          <div class="emoji">🌽</div>
+        </div>
+      </template>
 
-    <template #title> Advanced Card </template>
-    <template #content>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed
-      consequuntur error repudiandae numquam deserunt quisquam repellat libero
-      asperiores earum nam nobis, culpa ratione quam perferendis esse,
-      cupiditate neque quas!
-    </template>
-    <template #footer>
-      <p>Dato: 02.02.2022</p>
-    </template>
-  </Card>
+      <template #title>
+        <p class="text-left">Advanced Card with long tekst</p>
+      </template>
+      <template #content>
+        <p class="text-left">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore
+          sed consequuntur error repudiandae numquam deserunt quisquam repellat
+          libero asperiores earum nam nobis, culpa ratione quam perferendis
+          esse, cupiditate neque quas!
+        </p>
+      </template>
+      <template #footer>
+        <p class="text-right">Dato: 02.02.2022</p>
+      </template>
+    </Card></a
+  >
 </template>
 
 <script lang="ts">
@@ -31,13 +37,31 @@ export default defineComponent({
 });
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.p-card {
+  background-color: var(--card-background-color);
+  color: var(--card-text-color);
+}
+.p-card:hover,
+.p-card:focus,
+.p-card:active {
+  box-shadow: 0 8px 16px 0 var(--card-shadow);
+  opacity: 0.9;
+  filter: alpha(opacity=20);
+  /* For IE8 and earlier */
+}
+:deep(.p-card-title) {
+  color: var(--card-title-color);
+}
+:deep(.p-card-footer) {
+  color: var(--card-date-color);
+}
 .p-card .emoji-container {
   position: relative;
   display: inline;
 }
 .p-card .emoji {
-  background-color: pink;
+  background-color: var(--card-theme-circle);
   border-radius: 50%;
   font-size: 18px;
   font-weight: 700;
--- src/view/RecipeList.vue
+++ src/view/RecipeList.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="grid">
-    <div class="col-12 md:col-6 lg:col-3"><RecipeCard></RecipeCard></div>
-    <div class="col-12 md:col-6 lg:col-3"><RecipeCard></RecipeCard></div>
-    <div class="col-12 md:col-6 lg:col-3"><RecipeCard></RecipeCard></div>
-    <div class="col-12 md:col-6 lg:col-3"><RecipeCard></RecipeCard></div>
+    <div class="col-12 md:col-6 lg:col-4"><RecipeCard></RecipeCard></div>
+    <div class="col-12 md:col-6 lg:col-4"><RecipeCard></RecipeCard></div>
+    <div class="col-12 md:col-6 lg:col-4"><RecipeCard></RecipeCard></div>
+    <div class="col-12 md:col-6 lg:col-4"><RecipeCard></RecipeCard></div>
   </div>
 </template>
 
