3b9094976bf4c7eee0a7ee5877c1960d08f025cf
ceciliehrr <ceciliehrr@gmail.com>
2022-08-23 22:16:22 +0200

    astro 2

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,25 @@
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+        content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
+        theme: {
+            extend: {},
+            colors: {
+                transparent: 'transparent',
+                current: 'currentColor',
+                'white': '#ffffff',
+                'black': '#1f1209',
+                'vanilla': '#FDF8E8',
+                'green': {
+                    100: '#BED5AE',
+                    200: '#4a686d'
+
+                },
+                'pink': {
+                    100: '#FBD3C4',
+                    200: '#cfaebf',
+                    300: '#CE8884'
+
+                },
+                plugins: [],
+            }
+        }
+    }
