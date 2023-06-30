fb18fb92041b2e1c960338b9ec9d86d20e31757a
Cecilie Hansen Rørås <ceciliehrr@gmail.com>
2023-06-30 22:33:10 +0200

    fjern storyblok

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,6 @@ import image from "@astrojs/image";
 
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
-import storyblok from "@storyblok/astro";
-
-import { loadEnv } from "vite";
-
-const env = loadEnv("", process.cwd(), "STORYBLOK");
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,16 +12,6 @@ export default defineConfig({
     tailwind(),
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    storyblok({
-      accessToken: env.STORYBLOK_TOKEN,
-      components: {
-        // Add your components here
-      },
-      apiOptions: {
-        // Choose your Storyblok space region
-        region: "eu", // optional,  or 'eu' (default)
-      },
     }),
   ],
 });
