4fabd2ff2a22d455ab3a5744d78262de745fa226
Cecilie Hansen Rørås <ceciliehrr@gmail.com>
2023-09-21 17:34:10 +0200

    add ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
-
 import vue from "@astrojs/vue";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "hybrid",
   integrations: [vue()],
   image: {
     domains: ["https://foodstalker.b-cdn.net"],
@@ -24,4 +26,5 @@ export default defineConfig({
       },
     },
   },
+  adapter: netlify(),
 });
