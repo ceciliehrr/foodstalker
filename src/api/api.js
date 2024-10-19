211b6063f9473b4b195992394cd70ec3d5299381
Cecilie Hansen Rørås <ceciliehrr@gmail.com>
2024-10-19 13:16:03 +0200

    add visitors

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,6 @@
 export async function getTopList(apiKey, recipes) {
     try {
+      // Fetch data from Plausible API
       const response = await fetch(
         `https://plausible.io/api/v1/stats/breakdown?site_id=foodstalker.no&period=30d&property=event:page&limit=10`,
         {
@@ -11,28 +12,31 @@ export async function getTopList(apiKey, recipes) {
       );
   
       if (!response.ok) {
-        throw new Error('Failed to fetch top list');
+        throw new Error("Failed to fetch top list");
       }
   
+      // Parse response to JSON
       const pages = await response.json();
-  console.log(pages);
+  
+      // Regex pattern to extract the recipe ID from the URL
       const recipeIdRegex = /^\/oppskrift\/([^/]+)/;
   
-      // Extract recipe IDs and visitors
+      // Extract recipe IDs and visitor numbers
       const recipesData = pages.results
         .filter((page) => page.page.startsWith("/oppskrift/"))
         .map((page) => {
           const match = page.page.match(recipeIdRegex);
           if (match) {
-            const id = match[1]; // Extract the recipe ID from the URL
+            const id = match[1]; // Extract recipe ID from the URL
             return {
               id,
-              visitors: parseInt(page.visitors),
+              visitors: parseInt(page.visitors), // Parse visitor count to integer
             };
           }
           return null;
         })
-        .filter((page) => page !== null);
+        .filter((page) => page !== null); // Filter out any null entries
+  
   
       // Sort recipes by visitors in descending order
       recipesData.sort((a, b) => b.visitors - a.visitors);
@@ -40,13 +44,17 @@ export async function getTopList(apiKey, recipes) {
       // Take the top 5 recipes
       const topFiveRecipes = recipesData.slice(0, 5);
   
-      // Extract recipe IDs
-      const recipeIds = topFiveRecipes.map((recipe) => recipe.id);
-  
-      // Filter recipes based on the top 5 IDs
-      const sortedRecipes = recipeIds.map((id) =>
-        recipes.find((recipe) => recipe.id === id)
-      );
+      // Map recipe IDs to the actual recipes from the input recipes array
+      const sortedRecipes = topFiveRecipes.map(({ id, visitors }) => {
+        const recipe = recipes.find((recipe) => recipe.id === id);
+        if (recipe) {
+          return {
+            ...recipe, // Spread recipe details
+            visitors,  // Add the visitors count
+          };
+        }
+        return null;
+      }).filter(recipe => recipe !== null); // Filter out any null recipes
   
       return sortedRecipes;
     } catch (error) {
@@ -54,3 +62,4 @@ export async function getTopList(apiKey, recipes) {
       return [];
     }
   }
+  
